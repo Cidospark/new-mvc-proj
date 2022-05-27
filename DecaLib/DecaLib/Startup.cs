3eba1b7fc7101cb6a0be0484a1db9a6608642c01
Cidospark <cidospark27@gmail.com>
2022-05-27 16:34:40 +0100

    Connection string setuo

--- DecaLib/DecaLib/Startup.cs
+++ DecaLib/DecaLib/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DecaLib.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,7 @@ namespace DecaLib
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDataContext>(opt => opt.UseSqlite(Configuration.GetConnectionString("DefaultConn")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
